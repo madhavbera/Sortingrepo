7fb253f0699196109f56422aefab3e7f560fc9fe
Madhav <student@gmail.com>
2026-08-08 19:01:33 +0530

    Add sorting program 1

---
+++ bubble.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a[]={1,4,2,5,3};
+    int n=sizeof(a)/sizeof(a[0]);
+    printf("Display the given unsorted data:");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]<a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted array:");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+
+}
